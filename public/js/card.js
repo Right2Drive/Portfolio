c89af367c55954cc767a5d343f62ee4c0811302d
Right2Drive <bjpfannm@ucalgary.ca>
2016-09-21 19:16:38 -0400

    Added generation of cards via javascript

--- public/js/card.js
+++ public/js/card.js
@@ -16,12 +16,39 @@ function Card(name, images, contents) {
     this.loadLeft = function() {
 
     }
+
+    this.loadOnPage = function() {
+        var card = createCard("Future Blue Web Team");
+    }
 }
 
-function createCard() {
+function createCard(name) {
+    // Create the components of the card
     var card = document.createElement("DIV");
-    card.className = "cards-wrapper pure-g-r";
+    card.className = "card pure-u-19-24";
 
-    card.appendChild(document.createElement("div"));
+    var headerWrapper = document.createElement("DIV");
+    headerWrapper.className = "card-header pure-g-r";
 
+    var headerSpace = document.createElement("DIV");
+    headerSpace.className = "pure-u-1-24";
+
+    var titleWrapper = document.createElement("DIV");
+    titleWrapper.className = "pure-u-4-24";
+
+    var title = document.createElement("SPAN");
+    title.className = "card-title";
+    title.value = name;
+
+    var content = document.createElement("DIV");
+    content.className = "card-content-wrapper";
+
+    // Construct the element using the various components generated
+    titleWrapper.appendChild(title);
+    headerWrapper.appendChild(headerSpace);
+    headerWrapper.appendChild(titleWrapper)
+    card.appendChild(headerWrapper);
+    card.appendChild(content);
+
+    return card;
 }
