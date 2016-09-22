046b4107b1968ff87e3ec470cdd12a936b7cc8f8
Right2Drive <bjpfannm@ucalgary.ca>
2016-09-21 20:47:04 -0400

    Converting to module pattern

--- public/js/card.js
+++ public/js/card.js
@@ -2,60 +2,69 @@
 * This is my object oriented method of implementing my cards in javascript
  */
 
-// Using contructor pattern
-function Card(name, images, contents) {
-    this.name = name;
-    this.images = images;
-    this.contents = contents;
+var CARD_MOD = (function() {
+    var my = {};
 
+    //////////////////////// Private Methods  /////////////////////////////
 
-    this.loadRight = function() {
+    function createCard(name) {
+        // Create the components of the card
+        var card = document.createElement("DIV");
+        card.className = "card pure-u-19-24";
 
+        var headerWrapper = document.createElement("DIV");
+        headerWrapper.className = "card-header pure-g-r";
+
+        var headerSpace = document.createElement("DIV");
+        headerSpace.className = "pure-u-1-24";
+
+        var titleWrapper = document.createElement("DIV");
+        titleWrapper.className = "pure-u-4-24";
+
+        var title = document.createElement("SPAN");
+        title.className = "card-title";
+        title.value = name;
+
+        var content = document.createElement("DIV");
+        content.className = "card-content-wrapper";
+
+        // Construct the element using the various components generated
+        titleWrapper.appendChild(title);
+        headerWrapper.appendChild(headerSpace);
+        headerWrapper.appendChild(titleWrapper)
+        card.appendChild(headerWrapper);
+        card.appendChild(content);
+
+        return card;
     }
 
-    this.loadLeft = function() {
+    /////////////////////////// Card Object /////////////////////////////////
 
-    }
+    my.Card = function(name, images, contents) {
+        this.name = name;
+        this.images = images;
+        this.contents = contents;
 
-    this.loadOnPage = function() {
-        var card = createCard(this.name);
-        var section = document.getElementById('section-A');
-        var cardWrappers = section.getElementsByClassName('cards-wrapper');
-        if (cardWrappers.length === 1) {
-            cardWrappers[0].insertBefore(card, cardWrappers[0][3]);
-        } else {
-            throw "There are more than one 'cards-wrapper' classes per section";
+
+        this.loadRight = function() {
+
+        }
+
+        this.loadLeft = function() {
+
+        }
+
+        this.loadOnPage = function() {
+            var card = createCard(this.name);
+            var section = document.getElementById('section-A');
+            var cardWrappers = section.getElementsByClassName('cards-wrapper');
+            if (cardWrappers.length === 1) {
+                cardWrappers[0].insertBefore(card, cardWrappers[0][1]);
+            } else {
+                throw "There are more than one 'cards-wrapper' classes per section";
+            }
         }
     }
-}
 
-function createCard(name) {
-    // Create the components of the card
-    var card = document.createElement("DIV");
-    card.className = "card pure-u-19-24";
-
-    var headerWrapper = document.createElement("DIV");
-    headerWrapper.className = "card-header pure-g-r";
-
-    var headerSpace = document.createElement("DIV");
-    headerSpace.className = "pure-u-1-24";
-
-    var titleWrapper = document.createElement("DIV");
-    titleWrapper.className = "pure-u-4-24";
-
-    var title = document.createElement("SPAN");
-    title.className = "card-title";
-    title.value = name;
-
-    var content = document.createElement("DIV");
-    content.className = "card-content-wrapper";
-
-    // Construct the element using the various components generated
-    titleWrapper.appendChild(title);
-    headerWrapper.appendChild(headerSpace);
-    headerWrapper.appendChild(titleWrapper)
-    card.appendChild(headerWrapper);
-    card.appendChild(content);
-
-    return card;
-}
+    return my;
+}());
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
 /**
  * Created by Brad on 2016-09-21.
  */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+});
+
+// TODO if not supported (ie. IE 8 or below), output a message to user
+/*
+    FOR:
+        CSS VARIABLES
+        THE ABOVE EVENT LISTENER
+        TBC
+ */
